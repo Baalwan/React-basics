cb77d67656a191210f2e7c251b5daff79c3ff9a4
Baalwan <anshulbaalwan2016@gmail.com>
2025-03-12 01:04:43 +0530

    text utils updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
-import About from './Components/About';
+import Alert from './Components/Alert';
 import Navbar from './Components/Navbar';
-// import TextForm from './Components/TextForm';
+import TextForm from './Components/TextForm';
+// import About from './Components/About';
+import { useState } from 'react';
 
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = ()=>{
+    if (mode === 'dark'){
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+    }
+    else {
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743'
+      }
+    };
   return (
     <>
-      <Navbar title="Text Utils" aboutText="About Us"/>
+      {/* <Navbar title="Text Utils" aboutText="About Us"/> */}
+      <Navbar title= 'Text Utils' mode={mode} toggleMode={toggleMode}/>
+      <Alert alert= "This is alert"></Alert>
       <div className="container my-3">
-      {/* <TextForm heading="Enter the text to analyze"/> */}
-      <About/>
+      <TextForm heading="Enter your text here" mode= {mode}/>
+      {/* <About/> */}
       </div>
     </>
   );
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -13,20 +13,25 @@ export default function TextForm(props) {
     const handleOnChange =(event)=>{
         setText(event.target.value)
     }
+    const handleExtraSpaces =()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
 
     const [text, setText] = useState('');
     
     return (
         <>
-        <div>
-            <h1>{props.heading} </h1>
+        <div style={{color: props.mode==='dark'?'white':'black'}}>
+            <h1>{props.heading}  </h1>
                 <div className="mb-3">
-                    <textarea className="form-control" onChange={handleOnChange} value = {text} id="myBox" rows="8"/>
+                    <textarea className="form-control" style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} onChange={handleOnChange} value = {text} id="myBox" rows="8"/>
                 </div>
             <button type="submit" onClick = {handleUpClick} className="btn btn-primary mx-2">Convert to Uppercase</button>
             <button type="submit" onClick = {handleLowClick} className="btn btn-primary mx-2">Convert to Lowercase</button>
+            <button type="submit" onClick = {handleExtraSpaces} className="btn btn-primary mx-2">Remove Extra Spaces</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
             <h1>Your text Summary</h1>
             <p>Given text contains {text.split(" ").length} characters and {text.length} words.</p>
         </div>
